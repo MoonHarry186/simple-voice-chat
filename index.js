db6a8db0f73b136ed536f57e63d680faff9e2ce6
Harry's at Beae <phucnguyen@beae.com>
2024-09-26 16:17:17 +0700

    change dependency socket

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",  // Adjust this to your specific client URL if needed
+    methods: ["GET", "POST"],
+  },
+});
 
 //To holding users information 
 const socketsStatus = {};
